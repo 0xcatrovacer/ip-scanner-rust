4c03f720936172ff6d046ff2212d6ab494a45467
Swarnab Garang <swarnabgarang@gmail.com>
2022-01-18 13:40:05 +0530

    added scan function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::env;
-use std::net::IpAddr;
+use std::io::{self};
+use std::net::{IpAddr, TcpStream};
 use std::str::FromStr;
 use std::process;
+use std::sync::mpsc:: {Sender, channel};
+use std::thread;
+
+const MAX: u16 = 65535;
 
 struct Arguments {
     flag: String,
@@ -43,6 +48,26 @@ impl Arguments {
     }
 }
 
+fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut port: u16 = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+        }
+
+        if (MAX - port) <= num_threads {
+            break;
+        }
+        port += num_threads;
+    }
+}
+
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
@@ -57,5 +82,17 @@ fn main() {
             }
         }
     );
+
+    let num_threads = arguments.threads;
+    let (tx, rx) = channel();
+
+    for i in 0..num_threads {
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            scan(tx, i, arguments.ipaddr, num_threads);
+        });
+    }
+
 }
 
