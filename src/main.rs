e28bfda9f561777792a757960532a7bb7d5581eb
Swarnab Garang <swarnabgarang@gmail.com>
2022-01-18 12:49:00 +0530

    added multiple arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let flag = args[1].clone();
+    let threads = args[2].clone();
+    let ipaddr = args[3].clone();
+
+    for i in &args {
+        println!("{}", i);
+    }
+
+    println!("{:?}", args);
 }
+
